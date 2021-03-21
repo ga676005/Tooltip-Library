8e4e33c184e2a7162304b902005c0dd2994f9777
gohomewho <ga676005@hotmail.com.tw>
2021-03-22 00:53:28 +0800

    Build demo

--- script.js
+++ script.js
@@ -22,3 +22,40 @@ document.addEventListener("keydown", (e) => {
       break
   }
 })
+
+const settings = document.querySelector('.settings')
+const form = document.querySelector('.settings-modal')
+const element = document.querySelector('[data-tooltip]')
+
+
+settings.addEventListener('click', e => {
+  document.body.classList.add('open-settings')
+  const { spacing, positions, tooltip, bgColor, fgColor, fontSize, arrowSize, arrow, arrowDirection } = element.dataset
+
+  form.querySelector('#spacing').value = spacing
+  form.querySelector('#positions').value = positions
+  form.querySelector('#tooltip').value = tooltip
+  form.querySelector('#bgColor').value = bgColor
+  form.querySelector('#fgColor').value = fgColor
+  form.querySelector('#fontSize').value = fontSize
+  form.querySelector('#arrowSize').value = arrowSize
+  form.querySelector('#arrow').value = arrow
+  form.querySelector('#arrowDirection').value = arrowDirection
+
+})
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+
+  element.dataset.spacing = form.querySelector('#spacing').value
+  element.dataset.positions = form.querySelector('#positions').value
+  element.dataset.tooltip = form.querySelector('#tooltip').value
+  element.dataset.bgColor = form.querySelector('#bgColor').value
+  element.dataset.fgColor = form.querySelector('#fgColor').value
+  element.dataset.fontSize = form.querySelector('#fontSize').value
+  element.dataset.arrowSize = form.querySelector('#arrowSize').value
+  element.dataset.arrow = form.querySelector('#arrow').value
+  element.dataset.arrowDirection = form.querySelector('#arrowDirection').value
+
+  document.body.classList.remove('open-settings')
+})
